ad5e70e92fde54e100b569847ba6efb7280f0cac
martyopants@gmail.com <Marty OConnor>
2020-08-07 15:15:12 -0400

    Refactor to simplify getChoices()
    Previously getChoices used both async await and .then() statements.
    This refactoring using only async await.

--- src/block/pretty-words/utils/getChoices.js
+++ src/block/pretty-words/utils/getChoices.js
@@ -1,19 +1,14 @@
 const getChoices = async ( originalWord ) => {
 
-	const choices = await fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${originalWord}?key=${moconnorPrettyWordsEditor.dictionaryApiComKey }` )
-		.then( ( response ) => response.json() )
-		.then( ( json ) => {
+	const response = await fetch( `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${ originalWord }?key=${moconnorPrettyWordsEditor.dictionaryApiComKey }` );
+	const json = await response.json();
 
-			const arr = json[ 0 ].meta.syns[ 0 ].map(
-				( word ) => {
-					return { label: word, value: word };
-				}
-			);
+	return json[ 0 ].meta.syns[ 0 ].map(
+		( word ) => {
+			return { label: word, value: word };
+		}
+	);
 
-			return arr;
-		} );
-
-	return choices;
 };
 
 export default getChoices;
