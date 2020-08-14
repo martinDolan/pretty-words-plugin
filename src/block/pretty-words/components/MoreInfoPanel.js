7fbb08f68c849b4d42997a4ce47dfd32a1758822
martyopants@gmail.com <Marty OConnor>
2020-08-14 13:35:10 -0400

    Move code into GetDefinitions

--- src/block/pretty-words/components/MoreInfoPanel.js
+++ src/block/pretty-words/components/MoreInfoPanel.js
@@ -1,4 +1,3 @@
-import { Button, SelectControl, LinkControl, Panel, PanelBody, PanelRow } from '@wordpress/components';
 import GetDefinitions from '../utils/GetDefinitions';
 
 const MoreInfoPanel = ( props ) => {
@@ -7,16 +6,9 @@ const MoreInfoPanel = ( props ) => {
 
 	return (
 		<div>
-			<Panel >
-				<PanelBody title="More" initialOpen={ true }>
-					<PanelRow>
-						<div className="current-word">{ replacementWord }</div>
-						<div className="pronunciation">[ak-luh-meyt, uh-klahy-mit]</div>
-						<div className="part-of-speech">verb</div>
-						<div className="definition">to accustom or become accustomed to a new climate or environment; adapt.</div>
-					</PanelRow>
-				</PanelBody>
-			</Panel>
+			<GetDefinitions
+				replacementWord={ replacementWord }
+			/>
 		</div>
 	);
 
--- src/block/pretty-words/utils/GetDefinitions.js
+++ src/block/pretty-words/utils/GetDefinitions.js
@@ -1,6 +1,29 @@
 const GetDefinitions = () => {
 	console.log( 'getting definitions' );
+import { Button, SelectControl, LinkControl, Panel, PanelBody, PanelRow } from '@wordpress/components';
+
+const GetDefinitions = ( props ) => {
+
+	const { replacementWord } = props;
+
 	const response = fetch( `https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=${ moconnorPrettyWordsEditor.dictionaryApiDictionaryComKey }` );
+
+	response.then( ( response ) => response.json() )
+		.then( ( data ) => console.log( data[ 0 ] ) );
+	console.log( 'GetDefinitions' );
+	return (
+		<Panel >
+			<PanelBody title="More" initialOpen={ true }>
+				<PanelRow>
+					<div className="current-word">{ replacementWord }</div>
+					<div className="pronunciation">[ak-luh-meyt, uh-klahy-mit]</div>
+					<div className="part-of-speech">verb</div>
+					<div className="definition">to accustom or become accustomed to a new climate or environment; adapt.</div>
+				</PanelRow>
+			</PanelBody>
+		</Panel>
+	);
+
 };
 
 export default GetDefinitions;
