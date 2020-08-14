e9b2ded133702bf20bf33d5a64aa1aff22c3fc3f
martyopants@gmail.com <Marty OConnor>
2020-08-14 10:47:07 -0400

    Hard code word information

--- src/block/pretty-words/components/MoreInfoPanel.js
+++ src/block/pretty-words/components/MoreInfoPanel.js
@@ -3,12 +3,14 @@ import GetDefinitions from '../utils/GetDefinitions';
 
 const MoreInfoPanel = () => (
 	<>
-		<GetDefinitions />
+		{ /* <GetDefinitions /> */ }
 		<Panel >
 			<PanelBody title="More" initialOpen={ true }>
 				<PanelRow>
-					<div>Placeholderword</div>
-					<div>This is the definition of Placeholderword</div>
+					<div className="current-word">acclimate</div>
+					<div className="pronunciation">[ak-luh-meyt, uh-klahy-mit]</div>
+					<div className="part-of-speech">verb</div>
+					<div className="definition">to accustom or become accustomed to a new climate or environment; adapt.</div>
 				</PanelRow>
 			</PanelBody>
 		</Panel>
