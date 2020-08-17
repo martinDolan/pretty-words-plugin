1e3306c8c8dfc715e2fd75c446758024a1b3f325
martyopants@gmail.com <Marty OConnor>
2020-08-17 16:00:41 -0400

    Use props to render components dynamically

--- src/block/pretty-words/components/SingleDictionaryEntry.js
+++ src/block/pretty-words/components/SingleDictionaryEntry.js
@@ -1,12 +1,19 @@
 import { PanelRow } from '@wordpress/components';
 
-const SingleDictionaryEntry = () => {
+const SingleDictionaryEntry = ( props ) => {
+
+	const {
+		currentWord,
+		definition,
+		partOfSpeech,
+		pronunciation,
+	} = props;
 	return (
 		<PanelRow>
-			<div className="current-word">{ 'acclimate' }</div>
-			<div className="pronunciation">{ 'dictionaryInfo.hwi.hw' }</div>
-			<div className="part-of-speech">{ 'dictionaryInfo.fl' }</div>
-			<div className="definition">{ 'dictionaryInfo.shortdef' }</div>
+			<div className="current-word">{ currentWord }</div>
+			<div className="pronunciation">{ pronunciation }</div>
+			<div className="part-of-speech">{ partOfSpeech }</div>
+			<div className="definition">{ definition }</div>
 		</PanelRow>
 	);
 };
