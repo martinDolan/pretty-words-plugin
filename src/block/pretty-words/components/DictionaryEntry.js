2dec952e7e7a70246c06cf04fa4ea2bdfc182283
martyopants@gmail.com <Marty OConnor>
2020-08-17 15:27:18 -0400

    Hard-code two panel rows

--- src/block/pretty-words/components/DictionaryEntry.js
+++ src/block/pretty-words/components/DictionaryEntry.js
@@ -7,9 +7,15 @@ const DictionaryEntry = () => {
 			<PanelBody title="More" initialOpen={ true }>
 				<PanelRow>
 					<div className="current-word">{ 'acclimate' }</div>
-					<div className="pronunciation">[ak-luh-meyt, uh-klahy-mit]</div>
-					<div className="part-of-speech">verb</div>
-					<div className="definition">{ 'GetDefinition()' }</div>
+					<div className="pronunciation">{ 'dictionaryInfo.hwi.hw' }</div>
+					<div className="part-of-speech">{ 'dictionaryInfo.fl' }</div>
+					<div className="definition">{ 'dictionaryInfo.shortdef' }</div>
+				</PanelRow>
+				<PanelRow>
+					<div className="current-word">{ 'acclimate' }</div>
+					<div className="pronunciation">{ 'dictionaryInfo.hwi.hw' }</div>
+					<div className="part-of-speech">{ 'dictionaryInfo.fl' }</div>
+					<div className="definition">{ 'dictionaryInfo.shortdef' }</div>
 				</PanelRow>
 			</PanelBody>
 		</Panel>
